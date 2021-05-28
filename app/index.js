44dbe861ebe9c377f6c1d32d25953d9a6aebe1a3
Karla Vargas <kleandrav@gmail.com>
2021-05-28 14:01:00 +0800

    Dockerize

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 const appName = process.env.APPNAME;
 
 const express = require('express');
