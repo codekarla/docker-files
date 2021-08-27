fa711bfd32b7e7fddf04eb939e97701ec4336623
Karla Budgie <kleandrav@gmail.com>
2021-08-28 03:19:23 +0800

    somethings wrong

--- template-server-static/app/index.js
+++ template-server-static/app/index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const appName = process.env.APPNAME;
+const appName = process.env.APPNAME || 'CODE_BLITZ';
 
 const express = require('express');
 const app = express();
 const path = require('path');
 
 // set the path
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
-    res.render("index.html");
+    res.render('index.html');
 });
 
 app.listen(port, () => {
